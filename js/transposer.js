ac1353932a5520b8362beb73c238798c021e279f
Duncan Cai <dcai5@uwo.ca>
2014-12-24 19:02:35 -0500

    Using bootstrap containers for mobile

--- js/transposer.js
+++ js/transposer.js
@@ -3,66 +3,18 @@
 var flats = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "Cb"];
 var sharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 var keys = {
-  "C": {
-    index: 0,
-    sharps: 0,
-    flats: 0,
-  },
-  "Db": {
-    index: 1,
-    sharps: 0,
-    flats: 5,
-  },
-  "D": {
-    index: 2,
-    sharps: 2,
-    flats: 0,
-  },
-  "Eb": {
-    index: 3,
-    sharps: 0,
-    flats: 3,
-  },
-  "E": {
-    index: 4,
-    sharps: 4,
-    flats: 0,
-  },
-  "F": {
-    index: 5,
-    sharps: 0,
-    flats: 1,
-  },
-  "Gb": {
-    index: 6,
-    sharps: 0,
-    flats: 6,
-  },
-  "G": {
-    index: 7,
-    sharps: 1,
-    flats: 0,
-  },
-  "Ab": {
-    index: 8,
-    sharps: 0,
-    flats: 4,
-  },
-  "A": {
-    index: 9,
-    sharps: 3,
-    flats: 0,
-  },
-  "Bb": {
-    index: 10,
-    sharps: 0,
-    flats: 2,
-  },
-  "B": {
-    index: 11,
-    sharps: 5,
-    flats: 0,
-  },
+  "C": { index: 0, sharps: 0, flats: 0, },
+  "Db": { index: 1, sharps: 0, flats: 5, },
+  "D": { index: 2, sharps: 2, flats: 0, },
+  "Eb": { index: 3, sharps: 0, flats: 3, },
+  "E": { index: 4, sharps: 4, flats: 0, },
+  "F": { index: 5, sharps: 0, flats: 1, },
+  "Gb": { index: 6, sharps: 0, flats: 6, },
+  "G": { index: 7, sharps: 1, flats: 0, },
+  "Ab": { index: 8, sharps: 0, flats: 4, },
+  "A": { index: 9, sharps: 3, flats: 0, },
+  "Bb": { index: 10, sharps: 0, flats: 2, },
+  "B": { index: 11, sharps: 5, flats: 0,},
 };
 
 // Regex for recognizing chords
@@ -73,6 +25,9 @@ var colours = themes[0].colours;
  * Transposes text from one key to another.
  */
 function transpose(text) { 
+
+  // If empty string don't do anything
+  if(!text) return;
 
   var currentKey = $("#current-key").val();
   if(currentKey == "auto") {
